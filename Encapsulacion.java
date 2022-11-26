f0a3cdc7a947af69f98318d5424762f2f0aece24
Felipe Valenzuela <felipeivp@gmail.com>
2022-11-25 21:14:37 -0300

    Update Encapsulacion.java
    Final

--- Encapsulacion.java
+++ Encapsulacion.java
@@ -1,9 +1,54 @@
-public class Encapsulacion{
+public class Encapsulacion {
 
-    public static void main(String args) {
-        
-        
+    public static void main(String [] args) {
+
+        Persona persona = new Persona();
+        persona.setEdad(20);
+        persona.setNombre("Felipe");
+        persona.setTelefono(123456);
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
+
     }
 
+}
+
+class Persona {
+
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad) {
+
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(int telefono) {
+
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+
+        return edad;
+    }
+
+    public String getNombre() {
+
+        return nombre;
+    }
+
+    public int getTelefono() {
+
+        return telefono;
+    }
 
 }
